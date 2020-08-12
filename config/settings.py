ea1e8923cf176ee34399927d3d74f1ef7e861d14
shugo9538 <shugo9538@gmail.com>
2020-08-13 01:48:51 +0900

    test 3

--- config/settings.py
+++ config/settings.py
@@ -86,6 +86,8 @@ DATABASES = {
     }
 }
 
+DATABASES['default'].update(dj_database_url.config(conn_max_age=500))
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
