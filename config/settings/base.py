2f58f5efb0289c3317a5a203588e26acd9122643
shugo9538 <shugo9538@gmail.com>
2020-08-28 02:05:00 +0900

    changed static

--- config/settings/base.py
+++ config/settings/base.py
@@ -133,10 +133,9 @@ STATIC_URL = '/static/'
 
 # django_heroku(locals())
 
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    STATIC_DIR
+    os.path.join(BASE_DIR, 'static')
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
