23888147541c075332dfaf206a4459e25d8c47c1
shugo9538 <shugo9538@gmail.com>
2020-08-28 01:38:48 +0900

    changed

--- config/settings/production.py
+++ config/settings/production.py
@@ -5,10 +5,6 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*.herokuapp.com']
 
-DATABASES['default'] = dj_database_url.config()
-
 WSGI_APPLICATION = 'config.wsgi.production.txt.application'
-
-SITE_ID=1
 
 django_heroku.settings(locals())
